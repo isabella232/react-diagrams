01e0166b148c4302c0bc3e76b76b59374c830465
Rob Wilkinson <wilkinson.robert.a@gmail.com>
2017-03-23 13:34:50 -0700

    dont'need demos

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,11 @@
 /**
  * @author Dylan Vorster
  */
-module.exports = [
-	//for building the umd distribution
-	{
+module.exports = {
 		entry: './src/main.ts',
 		output: {
 			filename: 'main.js',
-			path: './dist',
+			path: __dirname +'/dist',
 			libraryTarget: 'umd',
 			library: 'storm-react-diagrams'
 		},
@@ -50,71 +48,4 @@ module.exports = [
 			extensions: [".tsx", ".ts", ".js"]
 		},
 		devtool: 'source-map'
-	},
-	//for building the demos and tests
-	{
-		entry: {
-			'demo1/bundle.js': './demos/demo1/index.ts',
-			'demo2/bundle.js': './demos/demo2/index.ts',
-			'demo3/bundle.js': './demos/demo3/index.ts',
-		},
-		output: {
-			filename: '[name]',
-			path: './demos',
-			libraryTarget: 'umd',
-			library: 'storm-react-diagrams'
-		},
-		externals: {
-			react: {
-				root: 'React',
-				commonjs2: 'react',
-				commonjs: 'react',
-				amd: 'react'
-			},
-			'react-dom': {
-				root: 'ReactDOM',
-				commonjs2: 'react-dom',
-				commonjs: 'react-dom',
-				amd: 'react-dom'
-			},
-			"lodash": {
-				commonjs: 'lodash',
-				commonjs2: 'lodash',
-				amd: '_',
-				root: '_'
-			}
-		},
-		module: {
-			rules: [
-				{
-					test: /\.scss$/,
-					use: [{
-						loader: "style-loader" // creates style nodes from JS strings
-					}, {
-						loader: "css-loader" // translates CSS into CommonJS
-					}, {
-						loader: "sass-loader" // compiles Sass to CSS
-					}]
-				},
-				{
-					enforce: 'pre',
-					test: /\.js$/,
-					loader: "source-map-loader"
-				},
-				{
-					test: /\.tsx?$/,
-					loader: 'ts-loader?' + JSON.stringify({
-						configFileName: 'tsconfig.json',
-						compilerOptions: {
-							declaration:false
-						}
-					}),
-				}
-			]
-		},
-		resolve: {
-			extensions: [".tsx", ".ts", ".js"]
-		},
-		devtool: 'source-map'
-	}
-];
+	};
